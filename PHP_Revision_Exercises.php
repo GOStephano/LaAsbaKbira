b6b9458b83cc76c621c823c8810b656659523085
GOStephano <60096607+GOStephano@users.noreply.github.com>
2020-03-31 20:08:45 +0200

    Add files via upload

---
+++ PHP_Revision_Exercises.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+-- Exercise 1 :
+
+    Create a database named 'car_seller';
+    Create a table, inside this DB, called 'Cars' with those attributs :
+        - id
+        - model
+        - manufacturer
+        - price
+        - photo
+        - type (Sedan, SUV and so on....)
+
+    You have to fill data (few cars will be enough)
+
+-- Exercise 2 :
+
+    Create a page called 'list-cars.php'
+    In this page, display every cars with matching informations and the picture(photo) of the car.
+
+-- Exercise 3 :
+
+    On the same page, near every car, add a 'edit' button.
+    This page should redirect to another page and send id (url looks like : http://localhost/edit-car.php?id=1)
+
+-- Exercise 4 :
+
+    Create a page called 'edit-car.php'
+    On this page, thanks to the id, you should be able to know which car you want to edit.
+    You have to display a form and every input should be filled with informations about the car.
+
+
+    When you press submit, it should edit informations in Database.
+
+
+-- Exercise 5 : 
+
+    On the 'list-cars' page, near every car, add a 'delete' button.
+    When you click on this button, it should delete the movie inside the Database and refresh the cars list.
+
+
+*/
+
+?>
